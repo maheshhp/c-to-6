041925aa4d32f6bca105bc69d26de2d9b38039e8
maheshhp <hpmahesh73@gmail.com>
2018-01-21 15:25:40 +0530

    Exercise six: Tests passing

--- spread.js
+++ spread.js
@@ -1,3 +1,11 @@
 function minFind(arg) {
-
+  if (typeof arg !== typeof []) {
+    return null;
+  }
+  let typeTest = arg.map((nArg) => { if (typeof nArg !== typeof 1) return "null"; return 1; }).toString().indexOf("null");
+  if (typeTest > -1) {
+    return null;
+  }
+  return Math.min(...arg);
 }
+module.exports = minFind;
